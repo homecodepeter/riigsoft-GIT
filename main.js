a57aa1926ee5da01cc8997c6bda9d92a9041ae81
homecodepeter <bookc8824@gmail.com>
2022-01-13 17:10:13 +0300

    add some to js code here

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
     var  year = 2022 - mo;
     var node = document.createTextNode(`${year} year's old`);
     // document.body.appendChild(node);
+    var name = "abdi";
+    var age =  56;
+    var second = "nasir";
+    console.log('i am ' + name + second + 'and iam ' + age +'old');
