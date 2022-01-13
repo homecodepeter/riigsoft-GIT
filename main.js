142c9b78a54661227adeedd68e6bd6fbc342818a
homecodepeter <bookc8824@gmail.com>
2022-01-13 16:57:59 +0300

    change js code here

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-class main {
-    
-}
+    var  mo = prompt("enter your bith ");
+    var  year = 2022 - mo;
+    var node = document.createTextNode(`${year} year's old`);
+    // document.body.appendChild(node);
